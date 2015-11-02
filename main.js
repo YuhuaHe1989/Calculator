21491ef205188ed1e1c22c28331dc12c3f2f797c
yuhua he <yuhuahe@buffalo.edu>
2015-11-01 17:14:08 -0800

    fixing merge conflict

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 (function(){
 
@@ -158,50 +157,3 @@ function multiply(){
 }
 
 })();
-
-
-
-
-
-=======
-$(document).ready(Calculator);
-
-function Calculator(){
-
-  $('.buttons').on('click',display);
-  $('.functions').on('click',operators);
-  $('.bt0').on('click',otherOperations)
-}
-
-function display(){
-
-  var number = $(this).text();
-
-  var inputs = $('#display').text() + number;
-  stack = stack.concat(inputs);
-  console.log(stack);
-  console.log(inputs);
-  $('#display').text(inputs);
-}
-
-function operators(){
-  var operator = $(this).text();
-
-  switch(operator) {
-    case '+':add(a,b); break;
-    case '-':substr(a,b); break;
-    case '*':multiply; break;
-    case '/':divide; break;
-    case '=':equal; break;
-  }
-}
-
-function add(a,b){
-  return a + b;
-}
-
-function otherOperations(){
-  var otherOperations = $(this).text();
-  console.log(otherOperations);
-}
->>>>>>> eedeed12c723bfeb96d83331238540156764f24c
